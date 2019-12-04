f751f753c4e250e27d6bfe920229de9465366324
JoseMi <jm.galeano1@gmail.com>
2019-12-03 23:06:44 -0500

    Update nytimes_API.py

--- nytimes_API.py
+++ nytimes_API.py
@@ -2,7 +2,7 @@
 # coding: utf-8
 
 import requests
-r = requests.get('https://api.nytimes.com/svc/community/v3/user-content/url.json?api-key=6xo5RmblUXnx5XIJaEgO03v7hpqfVI8O&offset=0&url=https%3A%2F%2Fwww.nytimes.com%2F2019%2F06%2F21%2Fscience%2Fgiant-squid-cephalopod-video.html')
+r = requests.get('https://api.nytimes.com/svc/community/v3/user-content/url.json?api-key={}&offset=0&url=https%3A%2F%2Fwww.nytimes.com%2F2019%2F06%2F21%2Fscience%2Fgiant-squid-cephalopod-video.html')
 r.json()
 
 import json
